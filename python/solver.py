cffbace2c13fd1ebfb78f8bba075908b35d14dc0
Rutger van de Lagemaat <zabilos@gmail.com>
2015-04-16 11:24:01 +0200

    working in 1.5 seconds, no optimizations since 15-04

--- python/solver.py
+++ python/solver.py
@@ -1,24 +1,54 @@
 game = {
-    'width': 17,
-    'height': 17,
+    'width': 55,
+    'height': 56,
     'tiles': [
-        { 'width': 4, 'height': 4 },
-        { 'width': 4, 'height': 4 },
-        { 'width': 2, 'height': 2 },
-        { 'width': 2, 'height': 2 },
-        { 'width': 2, 'height': 2 },
-        { 'width': 2, 'height': 2 }
-    ]
+ { 'width': 20, 'height': 21 },
+ { 'width': 19, 'height': 20 },
+ { 'width': 19, 'height': 18 },
+ { 'width': 18, 'height': 17 },
+ { 'width': 16, 'height': 17 },
+ { 'width': 16, 'height': 15 },
+ { 'width': 14, 'height': 15 },
+ { 'width': 14, 'height': 13 },
+ { 'width': 12, 'height': 13 },
+ { 'width': 12, 'height': 11 },
+ { 'width': 10, 'height': 11 },
+ { 'width': 9,  'height': 10 },
+ { 'width': 9,  'height': 8 },
+ { 'width': 7,  'height': 8 },
+ { 'width': 7,  'height': 6 },
+ { 'width': 6,  'height': 5 },
+ { 'width': 5,  'height': 4 },
+ { 'width': 4,  'height': 3 },
+ { 'width': 2,  'height': 3 },
+ { 'width': 1,  'height': 2 }
+ ]
 }
 class BaseSolver():
     def __init__(self, game):
         self.width = game['width']
         self.height = game['height']
         self.tiles = game['tiles']
-        self.matrix = [1,1,1,1,2,2,2,3,4,4,4]
-        # self.matrix = [0 for x in range(self.width)]
+        self.placedtiles = []
+        self.matrix = [0 for x in range(self.width)]
     def solve(self):
-        return 1;
+        if len(self.placedtiles) == len(self.tiles):
+            return True
+
+        row = self.findLowcation()
+        for index, tile in enumerate(self.tiles):
+            if tile != None and self.can_place(tile, row) == True:
+                self.place(tile, row)
+                self.placedtiles.append(tile)
+                self.tiles[index] = None
+                if self.solve():
+                    return True
+
+                self.placedtiles.pop()
+                self.tiles[index] = tile
+                self.remove(tile, row)
+        return False
+
     def findLowcation(self):
         lowest = min(self.matrix)
         lowlength = 0
@@ -51,9 +81,14 @@ class BaseSolver():
         return True
 
 b = BaseSolver(game)
-row = b.findLowcation()
-print 'row contains:', row
-place = b.place(b.tiles[0],b.findLowcation())
-print 'placing is completed:', place
-updatedrow = b.findLowcation()
-print 'row contains:', updatedrow
+solved = b.solve()
+
+print b.matrix, b.placedtiles
+
+
+# row = b.findLowcation()
+# print 'row contains:', row
+# place = b.place(b.tiles[0],b.findLowcation())
+# print 'placing is completed:', place
+# updatedrow = b.findLowcation()
+# print 'row contains:', updatedrow
