9be688b647c5c3f34bde0ba6a7810f4ef41b8676
Floris Stevens <florisst@live.nl>
2015-05-11 10:24:35 +0200

    game 17x17 in aparte file game 55x56 in aparte file game 6x8 in aparte file draai functie in solver niet als functie verslag

---
+++ python/games/17x17.py
@@ -0,0 +1,20 @@
+game = {
+    "width": 17,
+    "height": 17,
+    "tiles": [
+    { "width": 7, "height": 7 },
+    { "width": 7, "height": 7 },
+    { "width": 7, "height": 7 },
+    { "width": 5, "height": 5 },
+    { "width": 5, "height": 5 },
+    { "width": 5, "height": 5 },
+    { "width": 3, "height": 3 },
+    { "width": 3, "height": 3 },
+    { "width": 3, "height": 3 },
+    { "width": 3, "height": 3 },
+    { "width": 3, "height": 3 },
+    { "width": 3, "height": 3 },
+    { "width": 3, "height": 3 },
+    { "width": 2, "height": 2 }
+ ]
+}
---
+++ python/games/6x8.py
@@ -0,0 +1,12 @@
+  game = {
+   "width": 6,
+   "height": 8,
+   "tiles": [
+       { "width": 4, "height": 4 },
+       { "width": 4, "height": 4 },
+       { "width": 2, "height": 2 },
+       { "width": 2, "height": 2 },
+       { "width": 2, "height": 2 },
+       { "width": 2, "height": 2 }
+   ]
+}
